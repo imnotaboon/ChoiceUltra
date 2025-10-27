9198da38289c028bd173ecc3cabe9a5844b164b6
imnotaboon <imnotaboon@proton.me>
2025-10-27 19:25:36 +0100

    Add FHEVotingAddresses.ts

---
+++ voting-frontend/abi/FHEVotingAddresses.ts
@@ -0,0 +1,5 @@
+
+export const FHEVotingAddresses = { 
+  "11155111": { address: "0x259F3E6389cB14047ddC01fe4127A9d13FEd37f1", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x6AdE6B4297Ec10aa469a3001b3944868047c9bE9", chainId: 31337, chainName: "hardhat" },
+};
