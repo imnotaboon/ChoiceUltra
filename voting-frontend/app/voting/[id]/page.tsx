c727b1b2276f70421e0518ce7537122cb64cd6f0
imnotaboon <imnotaboon@proton.me>
2025-10-27 19:25:44 +0100

    Add page.tsx

---
+++ voting-frontend/app/voting/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { generateStaticParams } from "./generateStaticParams";
+import VotingDetailClient from "./VotingDetailClient";
+
+// Generate static params for static export
+export { generateStaticParams };
+
+export default function VotingDetailPage() {
+  return <VotingDetailClient />;
+}
