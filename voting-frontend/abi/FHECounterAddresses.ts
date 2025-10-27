24c53eb1c4cef0c2d5bbdd829cad97f5a5cbd118
imnotaboon <imnotaboon@proton.me>
2025-10-27 19:25:34 +0100

    Add FHECounterAddresses.ts

---
+++ voting-frontend/abi/FHECounterAddresses.ts
@@ -0,0 +1,5 @@
+
+export const FHECounterAddresses = { 
+  "11155111": { address: "0x6AdE6B4297Ec10aa469a3001b3944868047c9bE9", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x8B6380C99D3a2F31cC07eDdA882985746E1C305d", chainId: 31337, chainName: "hardhat" },
+};
